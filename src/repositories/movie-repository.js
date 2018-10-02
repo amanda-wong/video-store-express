06d4f7d3c51e840558bdcae9d1efe74b3fc2f316
Amanda <missamandawong@gmail.com>
2018-10-01 23:57:52 -0700

    Remove where class variable

--- src/repositories/movie-repository.js
+++ src/repositories/movie-repository.js
@@ -2,17 +2,16 @@ const db = require('./../../database/database');
 
 function getMovies(query,genre,callback) {
     var selectStatement = "select title from movie inner join genre on movie.genre_id = genre.id";  
-    var whereClause = '';
     
     if(query !== undefined && genre !== undefined) {
-        whereClause = ` where title similar to '%(${query})%' and genre = '${genre}'`;
+        selectStatement += ` where title similar to '%(${query})%' and genre = '${genre}'`;
     } else if (query !== undefined && genre === undefined) {
-        whereClause = ` where title similar to '%(${query})%'`;
+        selectStatement += ` where title similar to '%(${query})%'`;
     } else if(query === undefined && genre !== undefined) {
-        whereClause = ` where genre = '${genre}'`;
+        selectStatement += ` where genre = '${genre}'`;
     }
 
-    db.any(selectStatement + whereClause)
+    db.any(selectStatement)
         .then(function (data) {
             callback(null,data);
         })
